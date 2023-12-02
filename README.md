6c8c058eda49d7788304387b6b575bdbe08faefe
Wolffkran <liebherr2008@live.com>
2023-12-02 17:51:16 -0500

    Changed the readme

--- README.md
+++ README.md
@@ -1,20 +1,26 @@
 # 12 SQL: Employee Tracker
 
-## Your Task
-
-Developers frequently have to create interfaces that allow non-developers to easily view and interact with information stored in databases. These interfaces are called **content management systems (CMS)**. Your assignment this week is to build a command-line application from scratch to manage a company's employee database, using Node.js, Inquirer, and MySQL.
-
-Because this Challenge will require the use of the `Inquirer` package, ensure that you install and use Inquirer version 8.2.4. To do so, use the following command in your project folder: `npm i inquirer@8.2.4`.
-
-Because this application won’t be deployed, you’ll also need to create a walkthrough video that demonstrates its functionality and all of the following acceptance criteria being met. You’ll need to submit a link to the video and add it to the README of your project.
-
 ## User Story
 
 ```md
-AS A business owner
-I WANT to be able to view and manage the departments, roles, and employees in my company
-SO THAT I can organize and plan my business
+AS A company proprietor,
+I DESIRE the capability to oversee and administer the various departments, roles, and employees within my organization,
+SO THAT I can efficiently structure and strategize for the success of my business.
 ```
+## Description
+
+What was your motivation?
+
+- The motivation behind this project was to create a comprehensive command-line application that allows business owners or managers to efficiently manage their company's departments, roles, and employees. This tool aims to provide a user-friendly interface for organizing and planning business operations.
+Why did you build this project?
+
+- This project was built to address the need for a streamlined and centralized system for managing organizational structure. By developing a command-line application, users can perform essential tasks such as viewing, adding, updating, and removing departments, roles, and employees, thereby enhancing business efficiency.
+What problem does it solve?
+
+- The project solves the challenge of manual and time-consuming organization management by providing a digital solution for businesses. It offers a quick and effective way to organize departments, roles, and employees, facilitating better planning, coordination, and strategic decision-making.
+What did you learn?
+
+- During the development of this project, I gained practical experience in creating a command-line interface application and integrating it with a database. I learned about structuring and organizing database queries, handling user input, and implementing features that align with real-world business needs. Additionally, the project enhanced my understanding of how businesses can leverage technology for operational efficiency and organization management.
 
 ## Acceptance Criteria
 
--- db/index.js
+++ db/index.js
@@ -1,19 +1,19 @@
 const connection = require("./connection");
 
 class DB {
-  // Keeping a reference to the connection on the class in case we need it later
+  // Storing the connection reference on the class for potential future use
   constructor(connection) {
     this.connection = connection;
   }
 
-  // Find all employees, join with roles and departments to display their roles, salaries, departments, and managers
+  // Retrieve all employees, combining roles and departments to display their roles, salaries, departments, and managers
   findAllEmployees() {
     return this.connection.promise().query(
-      "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+      "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;"
     );
   }
 
-  // Find all employees except the given employee id
+  // Fetch all potential managers excluding the given employee id
   findAllPossibleManagers(employeeId) {
     return this.connection.promise().query(
       "SELECT id, first_name, last_name FROM employee WHERE id != ?",
@@ -21,12 +21,12 @@ class DB {
     );
   }
 
-  // Create a new employee
+  // Add a new employee
   createEmployee(employee) {
     return this.connection.promise().query("INSERT INTO employee SET ?", employee);
   }
 
-  // Remove an employee with the given id
+  // Delete an employee based on the given id
   removeEmployee(employeeId) {
     return this.connection.promise().query(
       "DELETE FROM employee WHERE id = ?",
@@ -34,7 +34,7 @@ class DB {
     );
   }
 
-  // Update the given employee's role
+  // Modify the role of the given employee
   updateEmployeeRole(employeeId, roleId) {
     return this.connection.promise().query(
       "UPDATE employee SET role_id = ? WHERE id = ?",
@@ -42,7 +42,7 @@ class DB {
     );
   }
 
-  // Update the given employee's manager
+  // Modify the manager of the given employee
   updateEmployeeManager(employeeId, managerId) {
     return this.connection.promise().query(
       "UPDATE employee SET manager_id = ? WHERE id = ?",
@@ -50,38 +50,38 @@ class DB {
     );
   }
 
-  // Find all roles, join with departments to display the department name
+  // Fetch all roles, combining with departments to display the department name
   findAllRoles() {
     return this.connection.promise().query(
-      "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+      "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department ON role.department_id = department.id;"
     );
   }
 
-  // Create a new role
+  // Add a new role
   createRole(role) {
     return this.connection.promise().query("INSERT INTO role SET ?", role);
   }
 
-  // Remove a role from the db
+  // Remove a role from the database
   removeRole(roleId) {
     return this.connection.promise().query("DELETE FROM role WHERE id = ?", roleId);
   }
 
-  // Find all departments
+  // Retrieve all departments
   findAllDepartments() {
     return this.connection.promise().query(
       "SELECT department.id, department.name FROM department;"
     );
   }
 
-  // Find all departments, join with employees and roles and sum up utilized department budget
+  // Retrieve all departments, combining with employees and roles, and summing up utilized department budget
   viewDepartmentBudgets() {
     return this.connection.promise().query(
-      "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id GROUP BY department.id, department.name;"
+      "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id GROUP BY department.id, department.name;"
     );
   }
 
-  // Create a new department
+  // Add a new department
   createDepartment(department) {
     return this.connection.promise().query("INSERT INTO department SET ?", department);
   }
@@ -94,18 +94,18 @@ class DB {
     );
   }
 
-  // Find all employees in a given department, join with roles to display role titles
+  // Retrieve all employees in a given department, combining with roles to display role titles
   findAllEmployeesByDepartment(departmentId) {
     return this.connection.promise().query(
-      "SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department department on role.department_id = department.id WHERE department.id = ?;",
+      "SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department department ON role.department_id = department.id WHERE department.id = ?;",
       departmentId
     );
   }
 
-  // Find all employees by manager, join with departments and roles to display titles and department names
+  // Retrieve all employees managed by a given manager, combining with departments and roles to display titles and department names
   findAllEmployeesByManager(managerId) {
     return this.connection.promise().query(
-      "SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.title FROM employee LEFT JOIN role on role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id = ?;",
+      "SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.title FROM employee LEFT JOIN role ON role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id = ?;",
       managerId
     );
   }
